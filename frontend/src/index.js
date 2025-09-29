6957291c5db35351d8462fe94cddabcbad75c8b0
surendra <surendra1234@gmail.com>
2025-09-30 00:51:14 +0530

    eight

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HashRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
-import Home from "./landing_page/Home/home"
+// import Home from "./landing_page/Home/home"
 import Search from "./landing_page/Search"
 import Navbar from "./landing_page/Navbar"
 import Footer from "./landing_page/Footer"
